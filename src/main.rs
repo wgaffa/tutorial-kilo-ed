dd024b53990c83ad471d91435c77c262eae77779
Patrik Maunus <subscription@skriptladan.se>
2022-07-01 01:23:42 +0200

    Entering raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read};
+
+use crossterm::terminal;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    terminal::enable_raw_mode()?;
+
+    for byte in io::stdin().bytes() {
+        match byte {
+            Ok(b'q') => break,
+            Ok(c) if c.is_ascii_control() => print!("{c}\r\n"),
+            Ok(c) => print!("{c} ('{}')\r\n", c as char),
+            _ => {},
+        }
+    }
+
+    terminal::disable_raw_mode()?;
+
+    Ok(())
 }
