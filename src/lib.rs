8aaac244f8892522d61fb96bdcac15598fdf9fbf
Patrik Maunus <subscription@skriptladan.se>
2022-07-01 13:21:22 +0200

    Refactor into smaller methods

--- src/lib.rs
+++ src/lib.rs
@@ -25,21 +25,11 @@ impl Editor {
 
     pub fn draw_rows<W: Write>(&self, writer: &mut W) -> io::Result<()> {
         for i in 0..self.size.rows {
-
             if i == (self.size.rows / 3) {
-                let mut message = format!("Kilo editor -- version {}", version!());
-                if message.len() > self.size.cols as usize {
-                    message.truncate(self.size.cols as usize);
-                }
+                let message = self.message();
+                let padding = self.padding(message.len() as u16);
 
-                let padding = (self.size.cols - message.len() as u16) / 2;
-                if padding > 0 {
-                    write!(writer, "~")?;
-                }
-
-                write!(writer, "{}", " ".repeat(padding.saturating_sub(1) as usize))?;
-
-                write!(writer, "{}", message)?;
+                write!(writer, "{}", padding + &message)?;
             } else {
                 write!(writer, "~")?;
             }
@@ -52,13 +42,35 @@ impl Editor {
 
         Ok(())
     }
+
+    fn padding(&self, message_len: u16) -> String {
+        let mut padding = String::new();
+        let pad_size = (self.size.cols - message_len) / 2;
+        if pad_size > 0 {
+            padding.push('~');
+        }
+
+        padding += &" ".repeat(pad_size.saturating_sub(1) as usize);
+        padding
+    }
+
+    fn message(&self) -> String {
+        let mut message = format!("Kilo editor -- version {}", version!());
+
+        if message.len() > self.size.cols as usize {
+            message.truncate(self.size.cols as usize);
+        }
+
+        message
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::version;
     use super::*;
+    use crate::version;
 
+    use quickcheck::TestResult;
     use quickcheck_macros::quickcheck;
 
     #[test]
@@ -71,6 +83,29 @@ mod tests {
     }
 
     #[quickcheck]
+    fn message_should_less_than_cols_size(cols: u16, rows: u16) -> bool {
+        let editor = Editor::new(cols, rows);
+
+        let message = editor.message();
+
+        message.len() <= cols as usize
+    }
+
+    #[quickcheck]
+    fn padding_should_start_with_tiled_given_positive_cols(size: (u16, u16)) -> TestResult {
+        let (rows, cols) = size;
+
+        if cols == 0 {
+            return TestResult::discard();
+        }
+
+        let editor = Editor::new(cols, rows);
+        let message = editor.message();
+
+        TestResult::from_bool(message.len() <= cols as usize)
+    }
+
+    #[quickcheck]
     fn draw_rows_prints_tildes_and_escape_sequences(cols: u16, rows: u16) -> bool {
         let editor = Editor::new(cols, rows);
 
@@ -79,7 +114,11 @@ mod tests {
 
         let top = "~\x1b[K\r\n".repeat(rows as usize / 3);
 
-        let mut message = if rows > 0 {format!("Kilo editor -- version {}", version!())} else {String::new()};
+        let mut message = if rows > 0 {
+            format!("Kilo editor -- version {}", version!())
+        } else {
+            String::new()
+        };
         message.truncate(cols as usize);
         let padding = {
             if rows == 0 {
